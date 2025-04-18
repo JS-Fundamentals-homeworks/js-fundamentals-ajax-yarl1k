166865b9b995131e7aa7ca525ea5a95b3cde8b34
yarl1k <yaroslawdyr@gmail.com>
2025-04-19 00:55:03 +0300

    completed

--- task1.js
+++ task1.js
@@ -1,7 +1,21 @@
-// Завдання: отримання даних про користувачів 
+// Завдання: отримання даних про користувачів
 // За допомогою засобі Fetch отримати інформацію про користувачів
-// за посиланням - https://jsonplaceholder.typicode.com/users 
+// за посиланням - https://jsonplaceholder.typicode.com/users
 // Імена користувачів відобразити в ненумерованому списку ul.usersList,
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
 // Перевірити за допомогою команди npm tests/task1.test.js 
+async function loadNames() {
+    const list = document.getElementsByClassName('usersList');
+    const url = 'https://jsonplaceholder.typicode.com/users';
+
+    const response = await fetch(url);
+    const users = await response.json();
+
+users.forEach(user => {
+    const new_li = document.createElement("li");
+    new_li.textContent = user.name;
+    list[0].appendChild(new_li);
+});
+}
+loadNames();
--- task2.js
+++ task2.js
@@ -5,3 +5,37 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+async function getUserInfo() {
+    const name_input = document.getElementById("userNameInput");
+    const user_button = document.getElementById("getUserButton");
+    const city_info = document.getElementById("userCity");
+
+    user_button.addEventListener('click', async function (e) {
+        const name = name_input.value.trim();
+        if (name === "" || name === null || typeof name === 'number') {
+            city_info.innerText = 'Введіть ім’я користувача';
+            return;
+        } 
+
+    try {
+        const url = 'https://jsonplaceholder.typicode.com/users'
+        const response = await fetch(url);
+        const users = await response.json();
+
+        const needed_user = users.find(
+            user => user.name.toLowerCase() === name.toLowerCase()
+        );
+        if (needed_user) {
+            city_info.innerText = needed_user.address.city;
+        }
+        else {
+            city_info.innerText = 'Користувача, нажаль, не знайдено';
+        }
+        
+    }
+    catch (e) {
+        console.log('Error');
+    }
+})
+}
+getUserInfo();
